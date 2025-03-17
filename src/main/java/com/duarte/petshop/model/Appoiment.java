94afe859f6c6aea43536cc2123bb128fa4b82ac8
Samuel Duarte <sduartegit@gmail.com>
2025-03-17 12:27:01 -0300

    feat: create enum for SpecieAnimal

---
+++ src/main/java/com/duarte/petshop/model/Appoiment.java
@@ -0,0 +1,12 @@
+package com.duarte.petshop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "appoiments")
+public class Appoiment {
+	
+	
+	//Implements
+}
